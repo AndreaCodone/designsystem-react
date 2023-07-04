b0f54a14668c03cc1a4e4d88e6c5c1d74bbd5cad
AndreaCodone <105943523+AndreaCodone@users.noreply.github.com>
2023-07-04 18:08:40 +0200

    Add files via upload

---
+++ 04-07-2023/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-1">SCSS</div>
+        </div>
+        <div className="row">
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+        </div>
+        <div className="row">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi
+            laboriosam repellat sed quidem vero amet, obcaecati architecto?
+            Illum exercitationem tempore facilis ullam, non neque expedita,
+            temporibus fuga veritatis qui dolorum!
+          </p>
+        </div>
+        <div className="row">
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+          <div className="col-2">col2</div>
+        </div>
+        <div className="row">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+            impedit sed qui, labore blanditiis quis perferendis animi
+            consequuntur consequatur laudantium molestiae autem. Quod laborum
+            doloremque placeat pariatur ipsam eaque dicta.
+          </p>
+        </div>
+        {/* <div className="row">
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+        </div>
+        <div className="row">
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+          <div className="col-4">col4</div>
+        </div>
+        <div className="row">
+          <div className="col-5">col5</div>
+          <div className="col-7">col7</div>
+        </div>
+        <div className="row">
+          <div className="col-12">col12</div>
+        </div>
+        <div className="row">
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-6">col6</div>
+        </div>
+        <div className="row space-between"></div>
+        <div className="row">
+          <div className="col-4">col4</div>
+          <div className="col-8">col8</div>
+        </div>
+        <div className="row">
+          <div className="col-xs-1">Col-xs-1</div>
+        </div>
+
+        <hr />
+        <hr />
+        <hr />
+        <hr /> */}
+        <div className="row">
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+          <div className="col-3">col3</div>
+        </div>
+        <div className="row">
+          <footer className="footer">Footer</footer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
